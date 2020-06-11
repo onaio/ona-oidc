f952807911de662be0e093eb7519c3507a2c7f78
Davis Raymond <davisraymondmuro@outlook.com>
2020-06-11 16:43:48 +0300

    Add pre commit (#1)
    * Add pre-commit
    * Lint files

--- oicd/urls.py
+++ oicd/urls.py
@@ -2,24 +2,22 @@
 URL Configuration file for ona-oicd
 """
 from django.urls import re_path
-
 from oicd.viewsets import OpenIDConnectViewset
-
 
 urlpatterns = [
     re_path(
         r"^oidc/(?P<auth_server>\w+)/login",
         OpenIDConnectViewset.as_view({"get": "login"}),
-        name="openid_connect_login"
+        name="openid_connect_login",
     ),
     re_path(
         r"^oidc/(?P<auth_server>\w+)/callback",
         OpenIDConnectViewset.as_view({"get": "callback", "post": "callback"}),
-        name="openid_connect_callback"
+        name="openid_connect_callback",
     ),
     re_path(
         r"^oidc/(?P<auth_server>\w+)/logout",
         OpenIDConnectViewset.as_view({"get": "logout"}),
-        name="openid_connect_logout"
+        name="openid_connect_logout",
     ),
 ]
