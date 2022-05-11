e7cb0e5ceb18c42b0ce07ab886bd475f5f3f13fb
Kipchirchir Sigei <arapgodsmack@gmail.com>
2022-05-11 13:16:06 +0300

    Move requirements to setup.cfg file
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
 """
 Setup file for ona-oidc
 """
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name="ona-oidc",
-    version=__import__("oidc").__version__,
-    description="A Django app that adds OpenID connect client functionality.",
-    license="Apache-2.0 License",
-    author="Ona Kenya",
-    url="https://github.com/onaio/ona-oidc",
-    packages=find_packages(exclude=["tests"]),
-    install_requires=[
-        "Django>=3.2.13,<4",
-        "djangorestframework",
-        "pyjwt[crypto]",
-        "requests",
-    ],
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.9",
-        "Framework :: Django",
-        "Framework :: Django :: 3.2.13",
-    ],
-)
+setuptools.setup()
