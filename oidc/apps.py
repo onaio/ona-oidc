ed7085c30c263151f609b34f74f5a84331685b6d
Morris Mukiri <morrismukiri@gmail.com>
2022-05-18 19:03:35 +0300

    switch from depracated ugettext
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>

--- oidc/apps.py
+++ oidc/apps.py
@@ -2,7 +2,7 @@
 Application Module for oidc app
 """
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class oidcConfig(AppConfig):
